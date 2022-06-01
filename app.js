d3c84c29d0fa57f0326294c843680f1dc5db9ebd
Thiago Luiz <thiagoluix21@gmail.com>
2022-06-01 00:57:38 -0300

    Landing Page commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// ScrollReveal
+window.sr = ScrollReveal({
+  reset: true,
+  duration: 2500,
+  distance: "60px"
+})
+
+sr.reveal('.image-ghiblis', { delay: 100 })
+sr.reveal('.informations', {
+  origin: "top"
+})
+// ScrollReveal
